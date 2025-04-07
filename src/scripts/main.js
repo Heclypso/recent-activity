84c1ff7c010f442cf94c42256ca2511d446cc9f2
Heclypso <heclypso@gmail.com>
2025-04-07 16:24:54 -0300

    #3 feat: desenvolvido tema de danger para o botão de testes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,18 @@
 import { chamarApi } from './package.min.js'
 
-document.querySelector('.btn-trigger').addEventListener('click', chamarApi)
+const button = document.querySelector('.btn-trigger')
+const buttonImage = document.querySelector('img.btn-trigger__image')
+
+button.addEventListener('click', function(){
+    chamarApi()
+    button.classList.toggle('btn-trigger--danger')
+
+    if (button.classList.contains('btn-trigger--danger')) {
+        buttonImage.src = "./images/danger-icon.svg"
+        buttonImage.alt = "Icone de conta em perigo"    
+    } else {
+        buttonImage.src = "./images/protected-icon.svg"
+        buttonImage.alt = "Icone de conta protegida"   
+    }
+}) 
+
